04e447b0d090570952eaf489b0f0e85ae9b4f545
Rubylium <alexisoko0@gmail.com>
2020-05-03 22:46:06 +0200

    Adding events to do action on other player inventory

--- server/player/inventory/srv_inv_events.lua
+++ server/player/inventory/srv_inv_events.lua
@@ -8,10 +8,25 @@ AddEventHandler("rF:GetPlayerInventory", function()
 end)
 
 
+RegisterNetEvent("rF:GetOtherPlayerInventory")
+AddEventHandler("rF:GetOtherPlayerInventory", function(target)
+    local player = _player_get_identifier(target)
+    local inv = PlayersData[player].inventory
+    TriggerClientEvent("rF:SendPlayerInventory", source, inv)
+end)
+
+
 RegisterNetEvent("rF:GiveItem")
 AddEventHandler("rF:GiveItem", function(token, item, count)
     if CheckToken(token, source) then
         AddItemToPlayerInv(source, item, count)
+    end
+end)
+
+RegisterNetEvent("rF:GiveItemToPlayer")
+AddEventHandler("rF:GiveItemToPlayer", function(token, target, item, count)
+    if CheckToken(token, source) then
+        AddItemToPlayerInv(target, item, count)
     end
 end)
 
@@ -20,5 +35,13 @@ RegisterNetEvent("rF:RemoveItem")
 AddEventHandler("rF:RemoveItem", function(token, item, count)
     if CheckToken(token, source) then
         RemoveItemFromPlayerInv(source, item, count)
+    end
+end)
+
+
+RegisterNetEvent("rF:RemoveItemToPlayer")
+AddEventHandler("rF:RemoveItemToPlayer", function(token, target, item, count)
+    if CheckToken(token, source) then
+        RemoveItemFromPlayerInv(target, item, count)
     end
 end)
