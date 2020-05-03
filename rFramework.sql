7967ceb4bf7ce81ab6e52fd64fece25a1d9303a5
Rubylium <alexisoko0@gmail.com>
2020-05-03 22:59:36 +0200

    Update rFramework.sql

--- rFramework.sql
+++ rFramework.sql
@@ -1,28 +1,76 @@
 CREATE DATABASE `rframework`;
 USE `rframework`;
 
-----------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 03, 2020 at 08:58 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 5.6.40
 
-CREATE TABLE `player_account` (
-  `id` int(11) NOT NULL,
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `omg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_account`
+--
+
+DROP TABLE IF EXISTS `player_account`;
+CREATE TABLE IF NOT EXISTS `player_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `player_first_spawn` varchar(255) NOT NULL DEFAULT 'first_spawn',
   `player_identifier` varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-  `player_health` varchar(255) NOT NULL DEFAULT '200', /* WIP*/
+  `player_health` varchar(255) NOT NULL DEFAULT '200',
   `player_position` text DEFAULT NULL,
-  `player_group` varchar(50) NOT NULL DEFAULT '0', /* WIP*/
-  `player_permission_level` int(11) NOT NULL DEFAULT '0', /* WIP */
-  `player_money` double NOT NULL DEFAULT '0',
-  `player_bank_balance` int(32) DEFAULT '0',
-  `player_dirty_money` double NOT NULL DEFAULT '0',
-  `player_job` varchar(50) NOT NULL DEFAULT 'police', /* WIP*/
-  `player_inv` text DEFAULT NULL /* WIP*/
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  `player_group` varchar(50) NOT NULL DEFAULT '0',
+  `player_permission_level` int(11) NOT NULL DEFAULT 0,
+  `player_money` double NOT NULL DEFAULT 0,
+  `player_bank_balance` int(32) DEFAULT 0,
+  `player_dirty_money` double NOT NULL DEFAULT 0,
+  `player_job` varchar(50) NOT NULL DEFAULT 'police',
+  `player_inv` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-----------------------------------------------------------
+-- --------------------------------------------------------
 
-ALTER TABLE `player_account`
-  ADD PRIMARY KEY (`id`);
+--
+-- Table structure for table `society`
+--
 
-ALTER TABLE `player_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+DROP TABLE IF EXISTS `society`;
+CREATE TABLE IF NOT EXISTS `society` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `society_name` varchar(35) NOT NULL,
+  `money` int(32) NOT NULL DEFAULT 0,
+  `inventory` text NOT NULL DEFAULT '{}',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `society`
+--
+
+INSERT INTO `society` (`id`, `society_name`, `money`, `inventory`) VALUES
+(1, 'exemple', 1500, '{}');
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
