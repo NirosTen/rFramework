e8e5c61ec4f50fd3595e1b921976f37b001b627f
Rubylium <alexisoko0@gmail.com>
2020-05-04 12:15:56 +0200

    Added ChangePlayerJobGrade

--- server/player/job/srv_jobHandler.lua
+++ server/player/job/srv_jobHandler.lua
@@ -4,6 +4,7 @@
 function ChangePlayerJob(id, job)
     local pCache = GetPlayerCache(id) 
     PlayersData[pCache].job = job
+    PlayersData[pCache].job_grade = 0
     TriggerClientEvent("rF:OnJobChange", id, job)
 end
 
@@ -12,5 +13,20 @@ RegisterNetEvent("rF:ChangePlayerJob")
 AddEventHandler("rF:ChangePlayerJob", function(tokenToCheck, job)
     if CheckToken(tokenToCheck, source) then
         ChangePlayerJob(source, job)
+    end
+end)
+
+
+function ChangePlayerJobGrade(id, grade)
+    local pCache = GetPlayerCache(id) 
+    PlayersData[pCache].job_grade = tonumber(grade)
+    TriggerClientEvent("rF:OnJobChange", id, job)
+end
+
+
+RegisterNetEvent("rF:ChangePlayerJobGrade")
+AddEventHandler("rF:ChangePlayerJobGrade", function(tokenToCheck, grade)
+    if CheckToken(tokenToCheck, source) then
+        ChangePlayerJobGrade(source, grade)
     end
 end)
