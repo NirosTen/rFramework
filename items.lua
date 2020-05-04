8a724f05dbe3238321e4b982d7f9137add3c171a
Rubylium <alexisoko0@gmail.com>
2020-05-04 17:29:38 +0200

    Starting items usage logics

--- items.lua
+++ items.lua
@@ -1,6 +1,6 @@
 
 -- All items with there weight
 items = {
-    {name="Bread", weight=0.3},
-    {name="Apple", weight=0.2},
+    {name="Bread", weight=0.3, event="rF:Eat_bread"},
+    {name="Apple", weight=0.2, event="rF:Eat_apple"},
 }
