08e7fbaaa795b6fe013703b80b5fa6284fe2e01a
Rubylium <alexisoko0@gmail.com>
2020-05-04 17:33:49 +0200

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,5 +1,5 @@
 
--- All items with there weight
+-- All items with there weight and event
 items = {
     {name="Bread", weight=0.3, event="rF:Eat_bread"},
     {name="Apple", weight=0.2, event="rF:Eat_apple"},
