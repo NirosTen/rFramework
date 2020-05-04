01292aea0614f6b4bbf86e9cd7a1ea17a19f2a2f
Rubylium <alexisoko0@gmail.com>
2020-05-04 17:29:52 +0200

    Exemple of items usage events

---
+++ client/items_events.lua
@@ -0,0 +1,10 @@
+
+RegisterNetEvent("rF:Eat_bread")
+AddEventHandler("rF:Eat_bread", function()
+    -- Do action if Bread is used
+end)
+
+RegisterNetEvent("rF:Eat_apple")
+AddEventHandler("rF:Eat_apple", function()
+    -- Do action if Apple is used
+end)
