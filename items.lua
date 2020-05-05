ecf2feb3cd0d31b56cc532df37bc464c39ac36b9
Rubylium <alexisoko0@gmail.com>
2020-05-05 15:52:24 +0200

    Changing default weight

--- items.lua
+++ items.lua
@@ -1,7 +1,7 @@
 
 -- All items with there weight and event
 items = {
-    {name="Bread", weight=0.3, event="rF:Eat_bread"},
-    {name="Apple", weight=0.2, event="rF:Eat_apple"},
+    {name="Bread", weight=0.7, event="rF:Eat_bread"},
+    {name="Apple", weight=0.5, event="rF:Eat_apple"},
     {name="CB", weight=0.0, event="core:UseCreditCard"}, -- Not used in the framework but in my core
 }
