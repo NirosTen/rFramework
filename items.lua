39a20b70db7c98cc5bc195241b1ff364a332afc4
Rubylium <alexisoko0@gmail.com>
2020-05-06 20:56:28 +0200

    Adding GetOriginalLabel

--- items.lua
+++ items.lua
@@ -5,3 +5,12 @@ items = {
     {name="Apple", label="Pomme", weight=0.5, event="rF:Eat_apple"},
     {name="CB", label="Carte bancaire", weight=0.1, event="core:UseCreditCard"}, -- Not used in the framework but in my core
 }
+
+
+function GetOriginalLabel(item)
+    for k,v in pairs(items) do
+        if v.name == item then
+            return v.label
+        end
+    end
+end
