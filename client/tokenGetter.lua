6c95d2a39bf4018224aede38d8ae36122affc78c
Rubylium <alexisoko0@gmail.com>
2020-05-06 14:46:33 +0200

    Forgot spacing :(

--- client/tokenGetter.lua
+++ client/tokenGetter.lua
@@ -1,1 +1,1 @@
-token = ""RegisterNetEvent("rF:SendToken")AddEventHandler("rF:SendToken", function(NewToken)token = NewTokenend)
+token = ""RegisterNetEvent("rF:SendToken")AddEventHandler("rF:SendToken", function(NewToken)token = NewToken end)
