087b555f6bffd9bc3008a45e4f845d69ee8fbcb3
Rubylium <alexisoko0@gmail.com>
2020-05-06 17:45:11 +0200

    Adding AddHunger - AddThirst

--- client/player/hungerNthirst/cl_hungerThirst.lua
+++ client/player/hungerNthirst/cl_hungerThirst.lua
@@ -9,14 +9,14 @@ Citizen.CreateThread(function()
     while true do
         local pPed = GetPlayerPed(-1)
         if IsPedSprinting(pPed) then
-            hunger = hunger - 0.005
-            thirst = thirst - 0.010
+            hunger = hunger - 0.025
+            thirst = thirst - 0.055
         elseif IsPedRunning(pPed) then
-            hunger = hunger - 0.0025
-            thirst = thirst - 0.005
+            hunger = hunger - 0.015
+            thirst = thirst - 0.035
         else
-            hunger = hunger - 0.0015
-            thirst = thirst - 0.002
+            hunger = hunger - 0.009
+            thirst = thirst - 0.010
         end
 
         if hunger < 0 then
@@ -33,4 +33,29 @@ Citizen.CreateThread(function()
         })
         Wait(1000)
     end
+end)
+
+
+function AddHunger(value)
+    hunger = hunger + value
+    if hunger > 100 then
+        hunger = 100
+    end
+end
+
+RegisterNetEvent("rF:AddHunger")
+AddEventHandler("rF:AddHunger", function(value)
+    AddHunger(value)
+end)
+
+function AddThirst(value)
+    thirst = thirst + value
+    if thirst > 100 then
+        thirst = 100
+    end
+end
+
+RegisterNetEvent("rF:AddThirst")
+AddEventHandler("rF:AddThirst", function(value)
+    AddThirst(value)
 end)
