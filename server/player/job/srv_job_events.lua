f4603264d31f6ffc11a4497087de1be439703e62
Rubylium <alexisoko0@gmail.com>
2020-05-07 21:49:40 +0200

    Adding srv_job_events

---
+++ server/player/job/srv_job_events.lua
@@ -0,0 +1,39 @@
+
+RegisterNetEvent("rF:ChangePlayerJob")
+AddEventHandler("rF:ChangePlayerJob", function(tokenToCheck, job)
+    if CheckToken(tokenToCheck, source) then
+        ChangePlayerJob(source, job)
+    end
+end)
+
+
+RegisterNetEvent("rF:ChangeOtherPlayerJob")
+AddEventHandler("rF:ChangeOtherPlayerJob", function(tokenToCheck, id, job)
+    if CheckToken(tokenToCheck, source) then
+        ChangePlayerJob(id, job)
+    end
+end)
+
+
+RegisterNetEvent("rF:ChangePlayerJobGrade")
+AddEventHandler("rF:ChangePlayerJobGrade", function(tokenToCheck, grade)
+    if CheckToken(tokenToCheck, source) then
+        ChangePlayerJobGrade(source, grade)
+    end
+end)
+
+RegisterNetEvent("rF:ResetPlayerJob")
+AddEventHandler("rF:ResetPlayerJob", function(tokenToCheck, id)
+    if CheckToken(tokenToCheck, source) then
+        ResetPlayerJob(id)
+    end
+end)
+
+
+
+RegisterNetEvent("rF:GetPlayerJob")
+AddEventHandler("rF:GetPlayerJob", function()
+    local job, grade = GetPlayerJob(source)
+    TriggerServerEvent("rF:JobRefresh", source, job, grade)
+    TriggerClientEvent("rF:job", id, job)
+end)
