e63ce783f3a9668528315c92cefd370288f1999f
Rubylium <alexisoko0@gmail.com>
2020-05-07 21:49:49 +0200

    Adding ResetPlayerJob

--- server/player/job/srv_jobHandler.lua
+++ server/player/job/srv_jobHandler.lua
@@ -10,13 +10,13 @@ function ChangePlayerJob(id, job)
 end
 
 
-RegisterNetEvent("rF:ChangePlayerJob")
-AddEventHandler("rF:ChangePlayerJob", function(tokenToCheck, job)
-    if CheckToken(tokenToCheck, source) then
-        ChangePlayerJob(source, job)
-    end
-end)
-
+function ResetPlayerJob(id)
+    local pCache = GetPlayerCache(id)
+    PlayersData[pCache].job = "Aucun"
+    PlayersData[pCache].job_grade = 0
+    TriggerClientEvent("rF:JobRefresh", id, job)
+    TriggerClientEvent("rF:job", id, job)
+end
 
 function ChangePlayerJobGrade(id, grade)
     local pCache = GetPlayerCache(id) 
@@ -26,22 +26,7 @@ function ChangePlayerJobGrade(id, grade)
 end
 
 
-RegisterNetEvent("rF:ChangePlayerJobGrade")
-AddEventHandler("rF:ChangePlayerJobGrade", function(tokenToCheck, grade)
-    if CheckToken(tokenToCheck, source) then
-        ChangePlayerJobGrade(source, grade)
-    end
-end)
-
-
 function GetPlayerJob(id)
     local pCache = GetPlayerCache(id) 
     return PlayersData[pCache].job, PlayersData[pCache].job_grade
 end
-
-RegisterNetEvent("rF:GetPlayerJob")
-AddEventHandler("rF:GetPlayerJob", function()
-    local job, grade = GetPlayerJob(source)
-    TriggerServerEvent("rF:JobRefresh", source, job, grade)
-    TriggerClientEvent("rF:job", id, job)
-end)
