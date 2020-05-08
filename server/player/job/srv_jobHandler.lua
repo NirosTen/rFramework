61977f2395611a8685113889c4286e39a81d710a
Rubylium <alexisoko0@gmail.com>
2020-05-08 09:04:33 +0200

    Fix: Sending false information from jobHandler

--- server/player/job/srv_jobHandler.lua
+++ server/player/job/srv_jobHandler.lua
@@ -5,7 +5,7 @@ function ChangePlayerJob(id, job)
     local pCache = GetPlayerCache(id) 
     PlayersData[pCache].job = job
     PlayersData[pCache].job_grade = 0
-    TriggerClientEvent("rF:JobRefresh", id, job)
+    TriggerClientEvent("rF:JobRefresh", id, PlayersData[pCache].job, PlayersData[pCache].job_grade)
     TriggerClientEvent("rF:job", id, job)
 end
 
@@ -14,14 +14,14 @@ function ResetPlayerJob(id)
     local pCache = GetPlayerCache(id)
     PlayersData[pCache].job = "Aucun"
     PlayersData[pCache].job_grade = 0
-    TriggerClientEvent("rF:JobRefresh", id, job)
+    TriggerClientEvent("rF:JobRefresh", id, PlayersData[pCache].job, PlayersData[pCache].job_grade)
     TriggerClientEvent("rF:job", id, job) 
 end
 
 function ChangePlayerJobGrade(id, grade)
     local pCache = GetPlayerCache(id) 
-    PlayersData[pCache].job_grade = tonumber(grade)
-    TriggerClientEvent("rF:JobRefresh", id, job)
+    PlayersData[pCache].job_grade = grade
+    TriggerClientEvent("rF:JobRefresh", id, PlayersData[pCache].job, PlayersData[pCache].job_grade)
     TriggerClientEvent("rF:job", id, job)
 end
 
