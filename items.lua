00b9c1bc2779f10e8a84ad0148e34034717f1dbf
Rubylium <alexisoko0@gmail.com>
2020-05-10 13:36:49 +0200

    What the fuck i did ?

--- items.lua
+++ items.lua
@@ -4,6 +4,8 @@ items = {
     {name="Bread", label="Pain", weight=0.7, event="rF:Eat_bread"},
     {name="Apple", label="Pomme", weight=0.5, event="rF:Eat_apple"},
     {name="CB", label="Carte bancaire", weight=0.1, event="core:UseCreditCard"}, -- Not used in the framework but in my core
+    {name="metal", label="Pièce métalique cassé", weight=1.5, event=""},
+    {name="metal2", label="Morceau de carrosserie", weight=1.5, event=""},
 }
 
 
