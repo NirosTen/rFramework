d92b188faf0bd66c3ef5fc3b5052baa19d3db123
Rubylium <alexisoko0@gmail.com>
2020-05-13 22:08:48 +0200

    Not something for you guys

--- items.lua
+++ items.lua
@@ -6,6 +6,12 @@ items = {
     {name="CB", label="Carte bancaire", weight=0.1, event="core:UseCreditCard"}, -- Not used in the framework but in my core
     {name="metal", label="Pièce métalique cassé", weight=1.5, event=""},
     {name="metal2", label="Morceau de carrosserie", weight=1.5, event=""},
+
+    -- Weapons
+    {name="pistolet", label="Pistolet", weight=5.0, event="core:GetPistol"},
+    {name="tazer", label="Tazer", weight=5.0, event="core:GetTazer"},
+    {name="pistoletmk2", label="Pistolet nouvelle génération", weight=5.0, event="core:GetPistolmk2"},
+    {name="pistoletlspd", label="Arme de poing LSPD", weight=5.0, event="core:GetPistollspd"},
 }
 
 
