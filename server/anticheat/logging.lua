8cc5c7df1366f52008bf84821df5985689c1e760
Rubylium <alexisoko0@gmail.com>
2020-05-16 22:54:59 +0200

    Adding UpdateIdentifiers to prevent cheater

--- server/anticheat/logging.lua
+++ server/anticheat/logging.lua
@@ -1,7 +1,13 @@
 local webhook = "https://discordapp.com/api/webhooks/711302645369012358/7EPYNlE3ujHDkwyif2jJ3q4IPWbIkugaj0eF7rY5ec8bqiAniYnYWx2g8A8DaJYX_kOu"
 
 
-local RaisonAfficher = "Active anti-cheat detection."
+BanList = {}
+Citizen.CreateThread(function()
+    BanList = LoadResourceFile(GetCurrentResourceName(), 'server/anticheat/bans.json')
+    BanList = json.decode(BanList)
+    print("^2Loaded^7 ban-list with "..#BanList.." bans.")
+end)
+
 
 
 function AddPlayerLog(id, log, force)
@@ -20,8 +26,41 @@ function AddPlayerLog(id, log, force)
 end
 
 function BanPlayer(id)
-
     SendLogToDiscord(id)
+    local logs = DetectionCache[id]
+    ban = {}
+    ban.name = GetPlayerName(id)
+    ban.ids = {}
+    ban.reason = {}
+    ban.date = os.date("%y/%m/%d %X")
+    for k,v in pairs(GetPlayerIdentifiers(id)) do
+        table.insert(ban.ids, v)
+    end
+    for k,v in pairs(logs.log) do
+        table.insert(ban.reason, v)
+    end
+    table.insert(BanList, ban)
+    SaveResourceFile(GetCurrentResourceName(), 'server/anticheat/bans.json', json.encode(BanList), -1)
+    print("^1BAN: ^7Added ["..id.."] to the ban-list.")
+    DropPlayer(id, "Vous avez été banni du serveur.\n"..ban.date)
+end
+
+
+function UpdateIdentifiers(k, identifiers)
+    for _,v in pairs(identifiers) do
+        local add = true
+        for _,i in pairs(BanList[k].ids) do
+            if v == i then
+                add = false
+            end
+        end
+        if add then
+            print("^1BAN-UPDATE: ^7Adding ["..v.."] to the ban-list.")
+            table.insert(BanList[k].ids, v)
+        end
+    end
+
+    SaveResourceFile(GetCurrentResourceName(), 'server/anticheat/bans.json', json.encode(BanList), -1)
 end
 
 
