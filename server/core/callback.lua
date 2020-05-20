dd323a88b425ee95376b7c68edee5285b14df2cf
Rubylium <alexisoko0@gmail.com>
2020-05-20 10:37:30 +0200

    Create callback.lua

---
+++ server/core/callback.lua
@@ -0,0 +1,31 @@
+local callbackDebug = true -- Display prints or not
+ServerCallbacks = {}
+
+function RegisterServerCallback(name, cb)
+    ServerCallbacks[name] = cb
+    if callbackDebug then
+        print("Callback "..name.." ^2REGISTERED^7")
+    end
+end
+
+function TriggerServerCallback(name, requestId, source, cb, ...)
+    if ServerCallbacks[name] then
+        if callbackDebug then
+            print("Callback "..name.." ^2CALLED^7 by "..source)
+        end
+		ServerCallbacks[name](source, cb, ...)
+	else
+		print(('^1ERROR:^7 Server callback "%s" does not exist.'):format(name))
+	end
+end
+
+
+
+RegisterServerEvent('triggerServerCallback')
+AddEventHandler('triggerServerCallback', function(name, requestId, ...)
+	local playerId = source
+
+	TriggerServerCallback(name, requestId, playerId, function(...)
+		TriggerClientEvent('serverCallback', playerId, requestId, ...)
+	end, ...)
+end)
