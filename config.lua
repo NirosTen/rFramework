d1988591bade2b2270d53687e2069c5c65a7f2fe
Rubylium <alexisoko0@gmail.com>
2020-05-20 15:27:16 +0200

    Changing default settings

--- config.lua
+++ config.lua
@@ -4,10 +4,10 @@ framework = {
 
     _display_logs = true, -- Display logs serveur (true / false)
 
-    _player_first_spawn_location = vector3(-1015.0816650391, -2752.2185058594,  0.80036240816116), -- First spawn location {x, y, z}
+    _player_first_spawn_location = vector3(389.2, -356.2, 48.02), -- First spawn location {x, y, z}
 
     _default_player_money = 2000, -- Default player money
-    _default_player_bank_balance = 3000, -- Default player bank balance
+    _default_player_bank_balance = 0, -- Default player bank balance
     _default_player_dirty_money = 0, -- Default player dirty money
     _default_player_job = 'Aucun',
     _default_player_group = "user",
