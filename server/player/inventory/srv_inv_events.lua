bf31ac46ad7b7b78aa7d1f762169d79ee0a84c75
Rubylium <alexisoko0@gmail.com>
2020-05-28 13:05:08 +0200

    Adding tokens check to events

--- server/player/inventory/srv_inv_events.lua
+++ server/player/inventory/srv_inv_events.lua
@@ -1,9 +1,11 @@
 
 
 RegisterNetEvent("rF:GetPlayerInventory")
-AddEventHandler("rF:GetPlayerInventory", function()
-    local weight = GetInvWeight(PlayersData[source].inventory)
-    TriggerClientEvent("rF:SendPlayerInventory", source, PlayersData[source].inventory, weight)
+AddEventHandler("rF:GetPlayerInventory", function(token)
+    if CheckToken(token, source, "GetPlayerInventory") then
+        local weight = GetInvWeight(PlayersData[source].inventory)
+        TriggerClientEvent("rF:SendPlayerInventory", source, PlayersData[source].inventory, weight)
+    end
 end)
 
 
@@ -14,9 +16,11 @@ end
 
 
 RegisterNetEvent("rF:GetOtherPlayerInventory")
-AddEventHandler("rF:GetOtherPlayerInventory", function(target)
-    local weight = GetInvWeight(PlayersData[target].inventory)
-    TriggerClientEvent("rF:SendPlayerInventory", source, PlayersData[target].inventory, weight)
+AddEventHandler("rF:GetOtherPlayerInventory", function(token, target)
+    if CheckToken(token, source, "GetOtherPlayerInventory") then
+        local weight = GetInvWeight(PlayersData[target].inventory)
+        TriggerClientEvent("rF:SendPlayerInventory", source, PlayersData[target].inventory, weight)
+    end
 end)
 
 RegisterServerCallback('rF:GetOtherPlayerData', function(source, cb, target)
@@ -26,14 +30,14 @@ end)
 
 RegisterNetEvent("rF:GiveItem")
 AddEventHandler("rF:GiveItem", function(token, item, count)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "GiveItem") then
         AddItemToPlayerInv(source, item, count)
     end
 end)
 
 RegisterNetEvent("rF:GiveItemToPlayer")
 AddEventHandler("rF:GiveItemToPlayer", function(token, target, item, count)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "GiveItemToPlayer") then
         AddItemToPlayerInv(target, item, count)
     end
 end)
@@ -41,7 +45,7 @@ end)
 
 RegisterNetEvent("rF:RemoveItem")
 AddEventHandler("rF:RemoveItem", function(token, item, count)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "RemoveItem") then
         RemoveItemFromPlayerInv(source, item, count)
     end
 end)
@@ -49,7 +53,7 @@ end)
 
 RegisterNetEvent("rF:RemoveItemToPlayer")
 AddEventHandler("rF:RemoveItemToPlayer", function(token, target, item, count)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "RemoveItemToPlayer") then
         RemoveItemFromPlayerInv(target, item, count)
     end
 end)
@@ -57,7 +61,7 @@ end)
 
 RegisterNetEvent("rF:BuyItemIfCan")
 AddEventHandler("rF:BuyItemIfCan", function(token, item, count, price)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "BuyItemIfCan") then
         BuyItemIfCanHoldIt(source, item, count, price)
     end
 end)
@@ -65,21 +69,21 @@ end)
 
 RegisterNetEvent("rF:RenameItem")
 AddEventHandler("rF:RenameItem", function(token, item, label, olabel)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "RenameItem") then
         RenameItem(source, item, label, olabel)
     end
 end)
 
 RegisterNetEvent("rF:ResetRenameItem")
 AddEventHandler("rF:ResetRenameItem", function(token, item, label, oldlabel)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "ResetRenameItem") then
         ResetRenameItem(source, item, label, oldlabel)
     end
 end)
 
 RegisterNetEvent("rF:TransferItemIfTargetCanHoldIt")
 AddEventHandler("rF:TransferItemIfTargetCanHoldIt", function(token, target, item, count, label, countSee)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "TransferItemIfTargetCanHoldIt") then
         TransferItemIfTargetCanHoldIt(source, target, item, count, label, countSee)
         RefreshPlayerInventory(target)
     end
@@ -88,7 +92,7 @@ end)
 RegisterNetEvent("rF:TransferItemIfTargetCanHoldItReverse")
 AddEventHandler("rF:TransferItemIfTargetCanHoldItReverse", function(token, target, item, count, label, countSee)
     print(token, target, item, count, label, countSee)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "TransferItemIfTargetCanHoldItReverse") then
         TransferItemIfTargetCanHoldItReverse(target, source, item, count, label, countSee)
         RefreshPlayerInventory(target)
     end
@@ -97,21 +101,21 @@ end)
 
 RegisterNetEvent("rF:AddItemIfNotAlreadyHave")
 AddEventHandler("rF:AddItemIfNotAlreadyHave", function(token, item, count)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "AddItemIfNotAlreadyHave") then
         AddItemIfNotAlreadyHave(source, item, count)
     end
 end)
 
 RegisterNetEvent("rF:ExhangeItem")
 AddEventHandler("rF:ExhangeItem", function(token, OriginalItem, ItemToGet)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "ExhangeItem") then
         ExhangeItem(source, OriginalItem, ItemToGet)
     end
 end)
 
 RegisterNetEvent("rF:SellItem")
 AddEventHandler("rF:SellItem", function(token, item, price)
-    if CheckToken(token, source) then
+    if CheckToken(token, source, "SellItem") then
         SellItem(source, item, price)
     end
 end)
