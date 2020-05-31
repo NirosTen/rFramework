abb420caa94541b01d26700e7d4af4aade8cdf59
Rubylium <alexisoko0@gmail.com>
2020-05-31 12:17:04 +0200

    Added ban and kick events

--- server/anticheat/commands.lua
+++ server/anticheat/commands.lua
@@ -29,9 +29,9 @@ end, true)
 
 RegisterCommand("ban", function(source, args, rawCommand)
     if source == 0 then
-        local target    = tonumber(args[1])
-        local duree     = tonumber(args[2])
-        local reason    = table.concat(args, " ",3)
+        local target = tonumber(args[1])
+        local duree = tonumber(args[2])
+        local reason = table.concat(args, " ",3)
         print(target, duree, reason)
         if target ~= nil then
             if duree == 0 then
@@ -43,9 +43,9 @@ RegisterCommand("ban", function(source, args, rawCommand)
             print("Aucun id spécifié.")
         end
     elseif PlayersCache[source].group ~= "user" then
-        local target    = tonumber(args[1])
-        local duree     = tonumber(args[2])
-        local reason    = table.concat(args, " ",3)
+        local target = tonumber(args[1])
+        local duree = tonumber(args[2])
+        local reason = table.concat(args, " ",3)
         if target ~= nil then
             if duree == 0 then
                 BanPlayer(target, reason, source)
@@ -55,3 +55,27 @@ RegisterCommand("ban", function(source, args, rawCommand)
         end
     end
 end, true)
+
+
+
+RegisterNetEvent("rF:BanPlayer")
+AddEventHandler("rF:BanPlayer", function(token, target, duree, reason)
+    if CheckToken(token, source, "rF:BanPlayer") then
+        local target = tonumber(target)
+        local duree = tonumber(duree)
+
+        if duree == 0 then
+            BanPlayer(target, reason, source)
+        else
+            TempBanPlayer(target, reason, duree, source)
+        end
+    end
+end)
+
+
+RegisterNetEvent("rF:KickPlayer")
+AddEventHandler("rF:KickPlayer", function(token, target, reason)
+    if CheckToken(token, source, "rF:BanPlayer") then
+        DropPlayer(target, reason)
+    end
+end)
