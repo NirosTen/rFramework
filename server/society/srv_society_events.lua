e0d01e3e86b93591e40e12c26d508c82824d1ad2
Rubylium <alexisoko0@gmail.com>
2020-06-07 11:43:18 +0200

    Added RemoveSocietyMoney net event

--- server/society/srv_society_events.lua
+++ server/society/srv_society_events.lua
@@ -5,6 +5,13 @@ AddEventHandler("rF:AddSocietyMoney", function(token, society, amount)
     end
 end)
 
+RegisterNetEvent("rF:RemoveSocietyMoney")
+AddEventHandler("rF:RemoveSocietyMoney", function(token, society, amount)
+    if CheckToken(token, source) then
+        AddSocietyMoney(society, amount)
+    end
+end)
+
 RegisterNetEvent("rF:PaySociety")
 AddEventHandler("rF:PaySociety", function(token, society, amount)
     if CheckToken(token, source) then
