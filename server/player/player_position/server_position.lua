af4094ae5e4ef75b4e5326c229c596a781e37fbc
Rubylium <alexisoko0@gmail.com>
2020-06-17 14:15:42 +0200

    Reworked pos logics

--- server/player/player_position/server_position.lua
+++ server/player/player_position/server_position.lua
@@ -25,13 +25,18 @@ function InitSpawnPlayer(source)
 
         end 
     else
-        local ToSpawnPos = json.decode(player_data[1].player_position)
-        local PosX = ToSpawnPos[1]
-        local PosY = ToSpawnPos[2]
-        local PosZ = ToSpawnPos[3]
-        TriggerClientEvent("rF:spawn_last_position", source, 1, PosX, PosY, PosZ)
-        if framework._display_logs == true then
-            print('' .. _L("user_reconnect") .. ' | '..player..' ' .. _L("user_reconnect2") .. '')
+        if player_data[1].player_position == nil then
+            local pos = framework._player_first_spawn_location
+            TriggerClientEvent("rF:spawn_last_position", source, 0, pos.x, pos.y, pos.z)
+        else
+            local ToSpawnPos = json.decode(player_data[1].player_position)
+            local PosX = ToSpawnPos[1]
+            local PosY = ToSpawnPos[2]
+            local PosZ = ToSpawnPos[3]
+            TriggerClientEvent("rF:spawn_last_position", source, 1, PosX, PosY, PosZ)
+            if framework._display_logs == true then
+                print('' .. _L("user_reconnect") .. ' | '..player..' ' .. _L("user_reconnect2") .. '')
+            end
         end
     end
 end
