eb61d313c0cb693e1de668313e3a73fa92e06469
Rubylium <alexisoko0@gmail.com>
2020-06-30 12:55:39 +0200

    Fix little id check

--- server/core/server.lua
+++ server/core/server.lua
@@ -41,10 +41,13 @@ function IsAllowedToJoin(id)
     local license = _player_get_identifier(id)
     local players = GetPlayers()
     for k,v in pairs(players) do
-        local oLicense = _player_get_identifier(v)
-        if license == oLicense then
-            print("^1CONNEXION ERROR:^7 Someone tried to join the server with the same license than someone else.")
-            return false
+        if tonumber(v) ~= tonumber(id) then
+            print(v, id)
+            local oLicense = _player_get_identifier(v)
+            if license == oLicense then
+                print("^1CONNEXION ERROR:^7 Someone tried to join the server with the same license than someone else.")
+                return false
+            end
         end
     end
     return true
